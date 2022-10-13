6629f42aa6e37bfd4e3757463ee64e05b5acf247
omar_kandil <93253683+OmarKandil-dw@users.noreply.github.com>
2022-10-13 05:03:59 -0700

    Delete prototype standard directory

--- prototype standard/bussines/edit.php
+++
@@ -1,9 +0,0 @@
-<?php
-    require '../data-access/promotion.php';
-    if(isset($_POST["edit"])){
-        $obj = new promotion($_POST["id_promotion"], $_POST["nom"]);
-        $obj->update();
-    }
-    header("location: ../presentation/promotion.php");
-
-?>
