2a9d8c1ddb61fac15feee3d720e05c2e4bde0a73
omar_kandil <93253683+OmarKandil-dw@users.noreply.github.com>
2022-10-14 02:39:04 -0700

    Update DB.sql

--- prototye_premium/data-access/DB.sql
+++ prototye_premium/data-access/DB.sql
@@ -1,10 +1,10 @@
-DROP DATABASE IF EXISTS brief1;
+DROP DATABASE IF EXISTS gestiondespromotions_v1;
 
-CREATE DATABASE brief1;
-USE brief1;
+CREATE DATABASE gestiondespromotions_v1;
+USE gestiondespromotions_v1;
 
 CREATE TABLE promotion(
-    id INT PRIMARY KEY AUTO_INCREMENT,
+    id_promotion INT PRIMARY KEY AUTO_INCREMENT,
     nom VARCHAR(25)
 );
-INSERT INTO promotion(nom) VALUES("promo1"),("promo2"),("promo3"),("promo4");
+INSERT INTO promotion(nom) VALUES("promotion  2022/2023"),;
